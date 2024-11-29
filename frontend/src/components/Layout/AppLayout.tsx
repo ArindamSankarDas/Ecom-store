f769ecffee3d6970dab72266d2ca81db96a852cc
Arindam Sankar Das <arindamsankar@outlook.com>
2024-11-29 19:37:20 +0530

    finish making footer component of the application

--- frontend/src/components/Layout/AppLayout.tsx
+++ frontend/src/components/Layout/AppLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 
 import Header from "@components/Header/Header";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
     <div id='layout' className='w-screen min-h-screen flex flex-col'>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
