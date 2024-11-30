a23314d285a2679b0cd494b2191323c8d3888328
Arindam Sankar Das <arindamsankar@outlook.com>
2024-11-30 20:55:52 +0530

    add React children element typing

--- frontend/src/components/ProductGrid/ProductGrid.tsx
+++ frontend/src/components/ProductGrid/ProductGrid.tsx
@@ -1,4 +1,10 @@
-const ProductGrid = ({ children }) => {
+import React, { ReactElement } from "react";
+
+type Props = {
+  children: ReactElement;
+};
+
+const ProductGrid: React.FC<Props> = ({ children }) => {
   return <div className='card-container p-5'>{children}</div>;
 };
 export default ProductGrid;
