92d625f681c0d85c10b1a8dc8d62280fd79c4895
Arindam Sankar Das <arindamsankar@outlook.com>
2024-11-30 20:50:03 +0530

    completion of Homepage

---
+++ frontend/src/components/ProductGrid/ProductGrid.tsx
@@ -0,0 +1,4 @@
+const ProductGrid = ({ children }) => {
+  return <div className='card-container p-5'>{children}</div>;
+};
+export default ProductGrid;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,56 @@
+import { Link } from "react-router-dom";
+import { Card, CardContent } from "@components/ui/card";
+
 import HeroSection from "@components/HeroSection/HeroSection";
+import ProductGrid from "@components/ProductGrid/ProductGrid";
+
+import featuredData from "@lib/data.json";
+import ProductCard from "@/components/ProductCard/ProductCard";
 
 const HomePage = () => {
+  const { featuredCategories, featuredProducts } = featuredData;
+
   return (
     <main className='flex-1 pb-10'>
       <HeroSection />
+      <section className='py-12'>
+        <div className='container mx-auto px-4 lg:px-20'>
+          <h2 className='text-3xl font-bold mb-8'>Featured Categories</h2>
+          <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
+            {featuredCategories.map((category) => (
+              <Link key={category.name} to={`/shop/${category.name}`}>
+                <Card>
+                  <CardContent className='p-4'>
+                    <div className='aspect-square relative mb-4'>
+                      <img
+                        src={category.thumbnail}
+                        alt={category.name}
+                        className='object-cover rounded-md'
+                      />
+                    </div>
+                    <h3 className='font-medium text-center'>{category.name}</h3>
+                  </CardContent>
+                </Card>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className='px-10'>
+        <h2 className='text-3xl font-bold mb-5'>Featured Products</h2>
+        <ProductGrid>
+          {featuredProducts.map((productItem) => (
+            <ProductCard
+              itemId={productItem.id}
+              itemCategory={productItem.category}
+              itemPic={productItem.thumbnail}
+              itemPrice={productItem.price}
+              itemTitle={productItem.title}
+            />
+          ))}
+        </ProductGrid>
+      </section>
     </main>
   );
 };
