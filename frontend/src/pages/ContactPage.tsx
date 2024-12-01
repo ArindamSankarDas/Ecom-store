e671c81d1b7bcfecc4990581484f0b0125df9142
Arindam Sankar Das <arindamsankar@outlook.com>
2024-12-01 20:44:39 +0530

    change styles to fit the website theme

--- frontend/src/pages/ContactPage.tsx
+++ frontend/src/pages/ContactPage.tsx
@@ -1,16 +1,16 @@
 import { Mail, Phone } from "lucide-react";
-import Input from "@components/Input/Input";
+import { Input } from "@components/ui/input";
 
 const ContactPage = () => {
   return (
-    <main className='border flex-1 py-5 flex flex-col items-center justify-center gap-7 lg:flex-row lg:px-10'>
+    <main className='border flex-1 py-5 flex flex-col items-center justify-center gap-7 lg:flex-row lg:px-10 lg:py-28'>
       <section
         id='text-info'
         className='border mx-6 p-8 shadow space-y-8 rounded-lg'
       >
         <section id='call-support' className='space-y-4'>
           <div className='flex items-center gap-2'>
-            <span className='bg-red-400 p-2 rounded-full'>
+            <span className='bg-black p-2 rounded-full'>
               <Phone color='white' />
             </span>
             <h1 className='font-semibold text-lg'>Call To US</h1>
@@ -23,7 +23,7 @@ const ContactPage = () => {
         <hr />
         <section id='email-support' className='space-y-4'>
           <div className='flex items-center gap-2'>
-            <span className='bg-red-400 p-2 rounded-full'>
+            <span className='bg-black p-2 rounded-full'>
               <Mail color='white' />
             </span>
             <h1 className='font-semibold text-lg'>Write to US</h1>
@@ -41,8 +41,16 @@ const ContactPage = () => {
         className='mx-5 px-6 py-8 border rounded-lg shadow flex flex-col gap-4 lg:mx-0'
       >
         <form className='grid-form' onSubmit={(e) => e.preventDefault()}>
-          <Input inputType='text' inputHolder='Your name' />
-          <Input inputType='email' inputHolder='Your Email' />
+          <Input
+            type='text'
+            placeholder='Your Name'
+            className='py-6 bg-gray-100 placeholder:text-gray-400'
+          />
+          <Input
+            type='email'
+            placeholder='Your Email'
+            className='py-6 bg-gray-100 placeholder:text-gray-400'
+          />
           <textarea
             cols={20}
             rows={5}
@@ -52,7 +60,7 @@ const ContactPage = () => {
         </form>
         <button
           type='submit'
-          className='w-fit self-end bg-red-400 text-white px-6 py-2 rounded-md font-semibold hover:cursor-pointer hover:bg-red-600 transition-all active:bg-red-400 shadow-md'
+          className='w-fit self-end bg-black border text-white px-6 py-2 rounded-md font-semibold hover:cursor-pointer hover:bg-white hover:text-black transition-all active:bg-gray-200 shadow-md'
         >
           Send Message
         </button>
